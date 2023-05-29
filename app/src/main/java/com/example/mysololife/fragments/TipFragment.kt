6596154ec66c64828f5bfd1eb736400860357c35
givesilverstick <silverstick21@sookmyung.ac.kr>
2023-05-29 17:03:49 +0900

    섹션4 - RecyclerView2까지

--- app/src/main/java/com/example/mysololife/fragments/TipFragment.kt
+++ app/src/main/java/com/example/mysololife/fragments/TipFragment.kt
@@ -1,5 +1,6 @@
 package com.example.mysololife.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +9,7 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.example.mysololife.R
+import com.example.mysololife.contentsList.ContentListActivity
 import com.example.mysololife.databinding.FragmentTipBinding
 
 class TipFragment : Fragment() {
@@ -24,6 +26,12 @@ class TipFragment : Fragment() {
     ): View? {
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_tip, container, false)
+
+        binding.cartegory1.setOnClickListener {
+
+            val intent = Intent(context, ContentListActivity::class.java)
+            startActivity((intent))
+        }
 
         binding.storeTap.setOnClickListener {
 
